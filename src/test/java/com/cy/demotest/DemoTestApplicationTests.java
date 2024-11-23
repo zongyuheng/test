68fff8a7c6f965dceff283b77e9a2a27d93bcccc
changyong <chang133122332@sina.com>
2024-11-23 12:39:33 +0800

    test

---
+++ src/test/java/com/cy/demotest/DemoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cy.demotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
